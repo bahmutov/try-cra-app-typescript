31244b8f04a96feb428bf359d59888b4a74b481a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-16 10:33:47 -0400

    try new mount plugin

--- src/App.cy-spec.tsx
+++ src/App.cy-spec.tsx
@@ -2,8 +2,9 @@
 /// <reference types="cypress-react-unit-test" />
 import React from 'react';
 import App from './App';
+import {mount} from 'cypress-react-unit-test'
 
 it('renders learn react link', () => {
-  cy.mount(<App />)
+  mount(<App />)
   cy.contains(/learn react/i)
 });
