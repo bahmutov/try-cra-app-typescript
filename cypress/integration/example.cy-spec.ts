aed44a77ec300d1f5af3639067ca4f86155385bb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-22 10:09:20 -0400

    update to v4

---
+++ cypress/integration/example.cy-spec.ts
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+describe('intergration', () => {
+  it('works', () => {
+    expect(1).to.equal(1)
+  })
+})
--- src/App.cy-spec.tsx
+++ src/App.cy-spec.tsx
@@ -1,9 +1,9 @@
 /// <reference types="cypress" />
-/// <reference types="cypress-react-unit-test" />
 import React from 'react';
 import App from './App';
+import {mount} from 'cypress-react-unit-test'
 
 it('renders learn react link', () => {
-  cy.mount(<App />)
+  mount(<App />)
   cy.contains(/learn react/i)
 });
